5c8803b3bbe3524ad97a717133aeb4daa76c175c
Kai <henning.kai@web.de>
2025-09-06 21:43:41 +0200

    update

--- scripts/add-packages.sh
+++ scripts/add-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 sudo mkdir -p /etc/live/build/package-lists/
-sudo cp chroot-configs/package-lists/surface-kde.list.chroot /etc/live/build/package-lists/
+sudo cp config/package-lists/surface-kde.list /etc/live/build/package-lists/
