446a31529a24d913a480fad3a83034fecc740170
Kai <henning.kai@web.de>
2025-09-06 15:20:47 +0200

    update

--- scripts/setup-archives.sh
+++ scripts/setup-archives.sh
@@ -11,9 +11,8 @@ deb http://deb.debian.org/debian bookworm-updates main contrib non-free
 deb http://security.debian.org/debian-security bookworm-security main contrib non-free
 EOF
 
-# Debian Archive Keys installieren
-sudo apt update
-sudo apt install -y debian-archive-keyring
+# Fehlende Debian GPG Keys importieren
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6ED0E7B82643E131 78DBA3BC47EF2265 F8D2585B8783D481 54404762BBB6E853 BDE6D2B9216EC7A8
 
 # Surface Linux GPG Key importieren
 wget -qO - https://pkg.surfacelinux.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/linux-surface-archive-keyring.gpg
