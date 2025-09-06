8d34e8b9cf5fe5a5a409df24f5d0792541039355
Kai <henning.kai@web.de>
2025-09-06 14:00:33 +0200

    update

--- scripts/setup-archives.sh
+++ scripts/setup-archives.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Alte Repo-Dateien entfernen
-rm -f /etc/apt/sources.list.d/*.list
+sudo rm -f /etc/apt/sources.list.d/*.list
 
 # Debian Repo
-cp config/archives/debian.list.chroot /etc/apt/sources.list
+sudo cp config/archives/debian.list.chroot /etc/apt/sources.list
 
 # Surface Repo GPG
 wget -qO - https://pkg.surfacelinux.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/linux-surface-archive-keyring.gpg
 
 # Surface Repo Liste
-cp config/archives/linux-surface.list.chroot /etc/apt/sources.list.d/linux-surface.list
+sudo cp config/archives/linux-surface.list.chroot /etc/apt/sources.list.d/linux-surface.list
