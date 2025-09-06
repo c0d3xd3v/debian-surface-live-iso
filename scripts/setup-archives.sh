e55dce17d5cffaac6b36c5a217acdc8c0bf6d1f2
Kai <henning.kai@web.de>
2025-09-06 14:07:27 +0200

    update

--- scripts/setup-archives.sh
+++ scripts/setup-archives.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
-# Alte Repo-Dateien entfernen
+# Löschen alter Listen, falls nötig
 sudo rm -f /etc/apt/sources.list.d/*.list
 
-# Debian Repo
-sudo cp config/archives/debian.list.chroot /etc/apt/sources.list
+# Debian main + updates + security
+sudo tee /etc/apt/sources.list > /dev/null <<EOF
+deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian bookworm-updates main contrib non-free
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free
+EOF
 
-# Surface Repo GPG
+# Linux Surface repo
 wget -qO - https://pkg.surfacelinux.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/linux-surface-archive-keyring.gpg
-
-# Surface Repo Liste
-sudo cp config/archives/linux-surface.list.chroot /etc/apt/sources.list.d/linux-surface.list
+sudo tee /etc/apt/sources.list.d/linux-surface.list > /dev/null <<EOF
+deb [arch=amd64 signed-by=/usr/share/keyrings/linux-surface-archive-keyring.gpg] https://pkg.surfacelinux.com/debian release main
+EOF
+sudo apt update
