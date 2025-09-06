79b6a6ce67eeb0099c200e51627dd39b1d439368
Kai <henning.kai@web.de>
2025-09-06 21:54:06 +0200

    update

--- scripts/enable-hooks.sh
+++ scripts/enable-hooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 sudo mkdir -p /etc/live/build/hooks/
-sudo cp chroot-configs/hooks/*.chroot /etc/live/build/hooks/
+sudo cp config/hooks/*.chroot /etc/live/build/hooks/
 sudo chmod +x /etc/live/build/hooks/*.chroot
